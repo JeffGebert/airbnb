77858d318dfa71939377b83bd57837cea106165d
JeffGebert <jeffgebert@hotmail.com>
2019-06-25 04:41:50 -0400

    added packages, index.js, script.js, db.js

---
+++ index.js
@@ -0,0 +1,26 @@
+// Config
+
+const express = require('express')
+const path = require('path')
+const app = express()
+
+// API
+
+app.get('/api/properties', require('./controllers/properties_get'))
+
+// Static Files
+
+
+// serve every file inside 'client' folder as static
+app.use(express.static(path.join(__dirname, 'client')))
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '/client/products.html'))
+	console.log('__dirname', __dirname)
+})
+
+// Run Server
+
+app.listen(3000, () => {
+	console.log('Server listening on port 3000');
+})
