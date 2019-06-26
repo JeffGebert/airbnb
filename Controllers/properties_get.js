130d0aa62ac7072ca4f629750d092bb2a6adb138
JeffGebert <jeffgebert@hotmail.com>
2019-06-26 01:31:57 -0400

    implemented double query

--- Controllers/properties_get.js
+++ Controllers/properties_get.js
@@ -10,34 +10,70 @@ ON properties.city = city.id
 LEFT JOIN country
 ON properties.country = country.id
 LEFT JOIN type
-ON properties.type = type.id`
+ON properties.type = type.id `
+
+	if (req.query && Object.keys(req.query).length) { // add WHERE if request has query
+		query += `WHERE `
+	}
+
+	console.log(Object.keys(req.query).length)
+	console.log(Object.keys(req.query)[0])
+	console.log(query.rating)
 
 	if (req.query.rating) {
-		query += ` WHERE properties.rating = ${req.query.rating}`
+		if (Object.keys(req.query)[0]=='rating'){
+			query += `properties.rating = ${req.query.rating}`
+		}else{
+			query += ` and properties.rating = ${req.query.rating}`
+		}
 	}
 	if (req.query.rooms) {
-		query += ` WHERE properties.rooms = ${req.query.rooms}`
+		if (Object.keys(req.query)[0]=='rooms'){
+			query += `properties.rooms = ${req.query.rooms}`
+		}else{
+			query += ` and properties.rooms = ${req.query.rooms}`
+		}
 	}
 	if (req.query.city) {
-		query += ` WHERE city.id = '${req.query.city}'`
+		if (Object.keys(req.query)[0]=='city'){
+			query += `city.id = '${req.query.city}'`
+		}else{
+			query += ` and city.id = '${req.query.city}'`
+		}
 	}
 	if (req.query.country) {
-		query += ` WHERE country.id = ${req.query.country}`
+		if (Object.keys(req.query)[0]=='country'){
+			query += `country.id = ${req.query.country}`
+		}else{
+		 	query += `and country.id = ${req.query.country}`
+		}
 	}
 
 	if (req.query.price) {
-		query += ` WHERE properties.price = ${req.query.price}`
+		if (Object.keys(req.query)[0]=='price'){
+			query += `properties.price = ${req.query.price}`
+		}else{
+			query += `and properties.price = ${req.query.price}`
+		}
 	}
 
 	if (req.query.plus) {
-		query += ` WHERE properties.plus = '${req.query.plus}'`
+		if (Object.keys(req.query)[0]=='plus'){
+			query += `properties.plus = '${req.query.plus}'`
+		}else{
+			query += `and properties.plus = '${req.query.plus}'`
+		}
 	}
 
 	if (req.query.roomtype) {
-		query += ` WHERE type.id = ${req.query.type}`
+		if (Object.keys(req.query)[0]=='type'){
+			query += `type.id = ${req.query.type}`
+		}else{
+			query += `and type.id = ${req.query.type}`
+		}
 	}
 
-
+	console.log('query', query)
 
 
 	db.query(query, (err, result) => {
